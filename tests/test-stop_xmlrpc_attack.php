128ad8611b464bedbcdf5db549fc2f9962daa5e6
Alfred Godoy <alfred@klandestino.se>
2015-01-26 14:34:49 +0100

    added tests for writing and removing our block to/from .htaccess

--- tests/test-stop_xmlrpc_attack.php
+++ tests/test-stop_xmlrpc_attack.php
@@ -2,8 +2,42 @@
 
 class StopXmlRpcAttackTest extends WP_UnitTestCase {
 
+	/**
+	 * FILTER METHODS WE NEED FOR TESTING:
+	 */
+
+	function returnEmptyArrayFilter($data) {
+		return array();
+	}
+
+	/**
+	 * THE TESTS
+	 */
+
 	function testClassIsInstanciated() {
 		$this->assertTrue(is_object($GLOBALS['plugin_stop_xmlrpc_attack']));
+	}
+
+	function testHtaccessPath() {
+		global $plugin_stop_xmlrpc_attack;
+		$this->assertEquals(rtrim(ABSPATH, '/') . '/.htaccess', $plugin_stop_xmlrpc_attack->htaccessFile);
+	}
+
+	function testWritingToHtaccessAndThenRemovingFromHtaccess() {
+		global $plugin_stop_xmlrpc_attack;
+		add_filter('stop_xmlrpc_attack_whitelist_arin_organizations', array($this, 'returnEmptyArrayFilter'));
+		do_action( 'stop_xmlrpc_attack_remove_htaccess' );
+		$data = file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile);
+		$this->assertFalse(strpos(file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile), '# BEGIN WORDPRESS PLUGIN stop_xmlrpc_attack'));
+		$this->assertFalse(strpos(file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile), '# END WORDPRESS PLUGIN stop_xmlrpc_attack'));
+		do_action( 'stop_xmlrpc_attack_generate_htaccess' );
+		$this->assertNotFalse(strpos(file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile), '# BEGIN WORDPRESS PLUGIN stop_xmlrpc_attack'));
+		$this->assertNotFalse(strpos(file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile), '# END WORDPRESS PLUGIN stop_xmlrpc_attack'));
+		do_action( 'stop_xmlrpc_attack_remove_htaccess' );
+		$data = file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile);
+		$this->assertFalse(strpos(file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile), '# BEGIN WORDPRESS PLUGIN stop_xmlrpc_attack'));
+		$this->assertFalse(strpos(file_get_contents($plugin_stop_xmlrpc_attack->htaccessFile), '# END WORDPRESS PLUGIN stop_xmlrpc_attack'));
+		remove_filter('stop_xmlrpc_attack_whitelist_arin_organizations', array($this, 'returnEmptyArrayFilter'));
 	}
 
 	function testRange2CIDR() {
